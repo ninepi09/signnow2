69aaf0c508a73f385abf247108155e1869f08e82
Rizki Mayandi <ninepilots063@gmail.com>
2021-02-26 11:08:18 +0700

    sef

--- src/App.svelte
+++ src/App.svelte
@@ -45,6 +45,8 @@
       const heroku = "https://cors-anywhere.herokuapp.com"; 
       
       const url = "https://192.168.1.21/tata_naskah/uploaded/sm/sm_15980_1.pdf"; 
+
+      const url40 = "https://192.168.1.40/tata_naskah/uploaded/sm/sm_15980_1.pdf"; 
       const uy = "https://yukmarry.com/sm_15431_1.pdf";   
       
       //const res = await fetch(proxyurl+pdftnde); // kalo pake cors anywhere
@@ -57,7 +59,7 @@
       console.log("pdfURL pakai cors dari 1.40", url); 
        
       // const res = await fetch(proxyurl+uy);
-      const res = await fetch(proxyurl+url);
+      const res = await fetch(proxyurl+url40);
       // const res = await fetch(pdfURL); // yang udah dinamic
       // const res = await fetch("/test.pdf");
       const pdfBlob = await res.blob();
